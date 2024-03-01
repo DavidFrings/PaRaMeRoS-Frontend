ad947f5f00863ab0e379cc0077aef7ae673514a1
PaRaMeRoS-Account <coolertyp99.development+github@gmail.com>
2024-03-01 23:13:49 +0000

    Added Pro Edition

--- src/templates/public/home.js
+++ src/templates/public/home.js
@@ -4,7 +4,7 @@ import Footer           from "../footers/footer";
 import Run              from "../../media/run.mp4";
 import Raum             from "../../media/raum.png";
 import About            from "../../media/about.png";
-import Ai               from "../../media/ai.png";
+import Ai               from "../../media/ai.png"
 import Hobbies          from "../../media/hobbies.png";
 import Login            from "../../media/login.png";
 import                       "../../css/home.css";
@@ -32,7 +32,7 @@ const home = () => {
                             zu halten. Tauchen Sie ein und erfahren Sie mehr über unsere Errungenschaften,
                             den fortschreitenden Entwicklungsprozess und andere relevante Aspekte, die einen
                             Einblick in die spannende Welt des PaRaMeRoS Teams gewähren.<br/><br/>
-                            <Link to="/events">Link to Events</Link>
+                            <Link to="/events">Link zu Events</Link>
                         </p>
                     </div>
                     <img
@@ -66,6 +66,21 @@ const home = () => {
                 <div className="info">
                     <div className="content">
                         <h3>
+                            Unser eigenes Betriebssystem & eigene Programmiersprache
+                        </h3>
+                        <p>
+                            Wir, als die Entwickler konzipierten diese Programmiersprache speziell für den Spike Prime, um die Verwendung von Micro Python zu vereinfachen und zu optimieren. Durch gezielte Verbesserungen und Anpassungen ermöglicht sie eine effizientere Programmierung für den Spike Prime, was insbesondere für Schüler und Lehrer von großem Vorteil ist, da sie unabhängig von Lego&#9415; den Hub <b>vollständig</b> ausschöpfen können. Die Sprache bietet eine benutzerfreundliche Syntax und umfasst Funktionen, die speziell auf die Bedürfnisse des Spike Prime zugeschnitten sind. Dadurch wird das Erstellen von Programmen erleichtert und die Lernkurve für Anfänger verkürzt. Darüber hinaus trägt die Entwicklung dieser Sprache dazu bei, die Möglichkeiten des Spike Prime voll auszuschöpfen und seine Leistungsfähigkeit zu maximieren, was zu einer verbesserten Erfahrung bei der Robotik Programmierung führt.<br/><br/>
+                            <Link to="https://github.com/Spike-Prime-Pro">Link zum Code</Link>
+                        </p>
+                    </div>
+                    <img
+                        src="https://www.derinformatikstudent.de/wp-content/uploads/2023/11/Firefly-Programmiersprachen-abstrakt-dargestellt-25549.jpg"
+                        alt="Pro"
+                    />
+                </div>
+                <div className="info">
+                    <div className="content">
+                        <h3>
                             Unsere Künstliche Intelligenz
                         </h3>
                         <p>
@@ -81,7 +96,8 @@ const home = () => {
                             Umwelteinflüsse zu minimieren. Diese fortschrittliche KI-Technologie geht über herkömmliche
                             Systeme hinaus, indem sie nicht nur die Effizienz steigert, sondern auch sicherstellt,
                             dass externe Einflüsse keinen negativen Einfluss auf die Funktionalität des Programms haben.
-                            Somit ermöglicht unser linear Backpropagation-KNN-System eine zuverlässige und robuste Leistung
+                            Somit ermöglicht unser linear Backpropagation-KNN-System eine zuverlässige und robuste
+                            Leistung
                             in unterschiedlichsten Umgebungen.
                         </p>
                     </div>
@@ -96,17 +112,28 @@ const home = () => {
                             Hobbies
                         </h3>
                         <p>
-                            Wenn Sie derzeit nach neuen Hobbys suchen oder einfach mehr über verschiedene Freizeitaktivitäten
-                            erfahren möchten, sind Sie hier genau richtig. In unserem Bereich finden Sie eine vielfältige
-                            Auswahl an Hobbys, die Ihnen dabei helfen können, Ihre Freizeit sinnvoll und unterhaltsam zu gestalten.
-                            Tauchen Sie ein und informieren Sie sich über die breite Palette an verfügbaren <Link to="/hobbies">Hobbies</Link>,
-                            die von kreativen Handwerksaktivitäten über sportliche Betätigungen bis hin zu intellektuellen Interessen
-                            reichen. Sollten Sie eigene Hobbys beisteuern wollen, haben Sie die Möglichkeit, dies einfach zu tun,
-                            indem Sie den Anweisungen auf der Seite folgen. Wir begrüßen und ermutigen Ihre Mitwirkung, um eine
-                            noch umfassendere Sammlung von Hobbys zu schaffen. Unser Ziel ist es, Ihnen bei der Entdeckung
-                            neuer Interessen zu helfen und Ihnen die Möglichkeit zu bieten, Ihre Leidenschaften zu teilen.
-                            Wir hoffen, dass diese Funktion für einige von Ihnen eine wertvolle Unterstützung darstellt und
-                            Ihnen dabei hilft, die perfekte Freizeitbeschäftigung zu finden. Genießen Sie die Vielfalt der
+                            Wenn Sie derzeit nach neuen Hobbys suchen oder einfach mehr über verschiedene
+                            Freizeitaktivitäten
+                            erfahren möchten, sind Sie hier genau richtig. In unserem Bereich finden Sie eine
+                            vielfältige
+                            Auswahl an Hobbys, die Ihnen dabei helfen können, Ihre Freizeit sinnvoll und unterhaltsam zu
+                            gestalten.
+                            Tauchen Sie ein und informieren Sie sich über die breite Palette an verfügbaren <Link
+                            to="/hobbies">Hobbies</Link>,
+                            die von kreativen Handwerksaktivitäten über sportliche Betätigungen bis hin zu
+                            intellektuellen Interessen
+                            reichen. Sollten Sie eigene Hobbys beisteuern wollen, haben Sie die Möglichkeit, dies
+                            einfach zu tun,
+                            indem Sie den Anweisungen auf der Seite folgen. Wir begrüßen und ermutigen Ihre Mitwirkung,
+                            um eine
+                            noch umfassendere Sammlung von Hobbys zu schaffen. Unser Ziel ist es, Ihnen bei der
+                            Entdeckung
+                            neuer Interessen zu helfen und Ihnen die Möglichkeit zu bieten, Ihre Leidenschaften zu
+                            teilen.
+                            Wir hoffen, dass diese Funktion für einige von Ihnen eine wertvolle Unterstützung darstellt
+                            und
+                            Ihnen dabei hilft, die perfekte Freizeitbeschäftigung zu finden. Genießen Sie die Vielfalt
+                            der
                             vorgestellten Hobbys und lassen Sie sich von der Welt der Freizeitaktivitäten inspirieren!
                         </p>
                     </div>
@@ -131,7 +158,8 @@ const home = () => {
                             zu finden ist. Diese leistungsstarke Funktion ermöglicht eine effiziente Steuerung und
                             Überwachung sowohl des Serverbetriebs als auch des Algorithmus, der für unser Team von
                             entscheidender Bedeutung ist. Die gut erreichbare Schnittstelle erleichtert die Interaktion
-                            mit dem Algorithmus und fördert eine reibungslose Kommunikation zwischen den Teammitgliedern.
+                            mit dem Algorithmus und fördert eine reibungslose Kommunikation zwischen den
+                            Teammitgliedern.
                             Insgesamt schafft unser Login eine dynamische Plattform, die nicht nur die täglichen
                             Teamaktivitäten erleichtert, sondern auch die technologische Effizienz und Kommunikation
                             innerhalb unseres Teams optimiert.
@@ -155,7 +183,8 @@ const home = () => {
                             Innovationsfähigkeiten zu fördern.
                         </p>
                     </div>
-                    <video controls loop controlsList="play nodownload" src={Run} typeof="video/mp4" autoPlay playsinline/>
+                    <video controls loop controlsList="play nodownload" src={Run} typeof="video/mp4" autoPlay
+                           playsinline/>
                 </div>
             </div>
             <Footer/>
